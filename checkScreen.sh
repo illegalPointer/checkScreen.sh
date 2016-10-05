a7743829e30334c38b3db6c0377dedab8306d53f
illegalPointer <illegalPointer@users.noreply.github.com>
2016-10-05 18:12:15 +0200

    Create checkScreen.sh

---
+++ checkScreen.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+YELLOW='\033[0;33m'
+GRAY='\033[0;37m'
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+
+while true;
+do
+  thereis=$(screen -ls | grep "No Sockets found")
+  if [ -n "${thereis}" ]:
+    then
+      tput bel
+      echo "${YELLOW}No screen sessions, scanning process have ended${GRAY}"
+  fi
+  sleep 1
+done
